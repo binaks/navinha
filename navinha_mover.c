93ef84e426155cc13cfc8632f1d90288382102c4
Bianca <bianca.rc@hotmail.com>
2016-11-25 02:02:53 -0300

    Add files via upload

--- navinha_mover.c
+++ navinha_mover.c
@@ -2,11 +2,34 @@
 
 void navinha_mover (Vector2 *nav, Vector2 posicaoNave) {
 
-    if (IsKeyDown(KEY_RIGHT)) posicaoNave.x += 4.0f;
-    if (IsKeyDown(KEY_LEFT)) posicaoNave.x -= 4.0f;
-    if (IsKeyDown(KEY_UP)) posicaoNave.y -= 4.0f;
-	if (IsKeyDown(KEY_DOWN)) posicaoNave.y += 4.0f;
+    if (IsKeyDown(KEY_RIGHT)) {
+    	if (posicaoNave.x < 790.0f) {
+    		posicaoNave.x += 5.0f;
+    	}
+    }
+
+    if (IsKeyDown(KEY_LEFT)) {
+    	if (posicaoNave.x > 12.0f) {
+    		posicaoNave.x -= 5.0f;
+    	}
+    }
+
+    if (IsKeyDown(KEY_UP)) {
+    	if (posicaoNave.y > 10.0f) {
+    		posicaoNave.y -= 5.0f;
+    	}
+    }
+
+	if (IsKeyDown(KEY_DOWN)) {
+    	if (posicaoNave.y < 440.0f) {
+    		posicaoNave.y += 5.0f;
+    	}
+	}
 
 	*nav = posicaoNave;
+
+	if (IsKeyDown(KEY_SPACE)) {
+		DrawCircleV(posicaoNave, 5, RAYWHITE);
+	}
 
 }
