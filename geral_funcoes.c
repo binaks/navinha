c9c3096edb21032f7dc1ee7164fe4ef89739c1b3
Bianca <bianca.rc@hotmail.com>
2016-11-27 18:34:02 -0300

    star_treko.c mais bonito

--- geral_funcoes.c
+++ geral_funcoes.c
@@ -18,6 +18,8 @@ static bool pause;
 
 static Nave nave;
 
+static Vector2 posicaoNave = {10.0f, 225.0f};
+static Vector2 *nav = &posicaoNave;
 
 //void Atirar (Vector2 posicaoTiro) {
 
@@ -25,7 +27,7 @@ static Nave nave;
 
 //}
 
-void IniciarJogo (Vector2 *nav, Vector2 posicaoNave) {
+void IniciarJogo () {
 	//Inicializa as variáveis do jogo
 
 	//Inicializa inimigos
@@ -50,11 +52,13 @@ void IniciarJogo (Vector2 *nav, Vector2 posicaoNave) {
 
 }
 
-void Update (Vector2 *nav, Vector2 posicaoNave) {
+void Update () {
 
 	if (!gameOver) {
 
 		if (IsKeyPressed('P')) pause = !pause;
+
+		Navinha_Desenhar(nav, posicaoNave);
 
 		if (!pause) {
 
@@ -63,11 +67,11 @@ void Update (Vector2 *nav, Vector2 posicaoNave) {
 
 		}
 
-		for (int i = 0; i < nave.vidas; i++) {
+/*		for (int i = 0; i < nave.vidas; i++) {
 			BeginDrawing();
 				DrawRectangle (20, 770, 35, 10, LIGHTGRAY);
 			EndDrawing();
-		}
+		} */
 	}
 
 }
--- star_treko.c
+++ star_treko.c
@@ -1,26 +1,15 @@
 #include "raylib.h"
 #include "geral_funcoes.c"
 
-int main()
-{
-    Vector2 posicaoNave = {10.0f, 225.0f};
-    //inicialização da posição da nave
+int main() {
 
-    Vector2 *nav;
-    nav = &posicaoNave;
+    IniciarJogo();
 
-    IniciarJogo(nav, posicaoNave);
-
-    // Main game loop
     while (!WindowShouldClose()) {    // Detect window close button or ESC key
 
-        Navinha_Desenhar(nav, posicaoNave);
-
-        Update(nav, posicaoNave);
+        Update();
 
     }
-
-    // De-Initialization
  
     CloseWindow();        // Close window and OpenGL context
 
