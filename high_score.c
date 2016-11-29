bc911e4c6ea5e86d37c7f8320e0734494b0fe409
Bianca <bianca.rc@hotmail.com>
2016-11-29 19:13:40 -0300

    oooops

--- high_score.c
+++ high_score.c
@@ -8,7 +8,8 @@ void High_Score (int high_score, int score) {
         ClearBackground(BLACK);
         
         if (score >= high_score) {
-            DrawText(FormatText("NEW HIGH SCORE"), 160, 100, 50, DARKBLUE);
+            DrawText(FormatText("NEW HIGH SCORE"), 160, 100, 50, MAROON);
+            DrawText(FormatText("Pressione ENTER para jogar novamente"), 90, 300, 30, DARKBLUE);
         } else {
             DrawText(FormatText("SCORE: %i", score), 290, 130, 40, GRAY);
             DrawText(FormatText("HI-SCORE: %i", high_score), 220, 200, 50, MAROON);
