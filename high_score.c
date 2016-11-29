29b2aa37ddbbe7d65ce15d4e45b9412c31a51f5d
Bianca <bianca.rc@hotmail.com>
2016-11-29 19:09:13 -0300

    final

--- high_score.c
+++ high_score.c
@@ -1,15 +1,19 @@
 #include "raylib.h"
+#include "stdio.h"
 
 void High_Score (int high_score, int score) {
 
     BeginDrawing();
 
         ClearBackground(BLACK);
-
-        DrawText(FormatText("SCORE: %i", score), 280, 130, 40, MAROON);
-        DrawText(FormatText("HI-SCORE: %i", high_score), 210, 200, 50, GRAY);
         
-        DrawText("Pressione ENTER para salvar", 250, 310, 20, LIGHTGRAY);
+        if (score >= high_score) {
+            DrawText(FormatText("NEW HIGH SCORE"), 160, 100, 50, DARKBLUE);
+        else {
+            DrawText(FormatText("SCORE: %i", score), 290, 130, 40, GRAY);
+            DrawText(FormatText("HI-SCORE: %i", high_score), 220, 200, 50, MAROON);
+            DrawText(FormatText("Pressione ENTER para jogar novamente"), 90, 300, 30, DARKBLUE);
+        }
 
     EndDrawing();
 }
